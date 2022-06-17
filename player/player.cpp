a7772c6459f206c10ec6345367a77c9dac27e0fb
ocar1053 <ocar8951@gmail.com>
2022-06-17 18:39:56 +0800

    6/17

--- player/player.cpp
+++ player/player.cpp
@@ -1,68 +1,48 @@
-#include <iostream>
-#include <cstdlib>
-#include <utility>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
-typedef pair<int, int> PII;
 
-#define F first
-#define S second
-#define mkp make_pair
+class Robot {
+    char** board;
+    int round;
+    int width, height;
+    pair<int, int> ALocation, BLocation;
+    public:
+        Robot(int robot, int width,int height);
+        void init(); 
+        bool validatePosition(int x, int y);
+    
+    
+};
 
-int Round, M, N;
-char Graph[20][20]={};
-
-bool valid(int x, int y){
-    return (
-        x >= 0 && x < M && y >=0 && y < N && 
-        Graph[x][y] != 'x' && 
-        Graph[x][y] != 'A' && Graph[x][y] != 'B'
-    );
-}
-
-string next_dir(int x, int y, int num){
-    for(int i=0;i<4;i++){
-        switch(num%4) {
-            case 0: 
-                if(valid(x-1, y))
-                    return "UP";
-                else break;
-            case 1:
-                if(valid(x+1, y)) 
-                    return "DOWN";
-                else break;
-            case 2: 
-                if(valid(x, y-1))
-                    return "LEFT";
-                else break;
-            case 3: 
-                if(valid(x, y+1))
-                    return "RIGHT";
-                else break;
-            default: return "RIGHT";
+void Robot::init() {
+    board = new char*[height];
+    for (int i = 0; i < height; i++) {
+        board[i] = new char[width];
+        for (int j = 0; j < width; j++) {
+            cin >> board[i][j];
+            if (board[i][j] == 'A') ALocation = make_pair(j, i);
+            if (board[i][j] == 'B') BLocation = make_pair(j, i);
         }
-        num++;
     }
-    return "RIGHT";
 }
 
-int main(void){
-    int A, B;
-    char me;
-    cin >> Round >> M >> N;
-    for(int i=0;i<M;i++)
-        for(int j=0;j<N;j++)
-            cin >> Graph[i][j];
-    cin >> A >> B >> me;
-    int x, y, rand_seed;
-    for(int i=0;i<M;i++)
-        for(int j=0;j<N;j++)
-            if(Graph[i][j] == me){
-                x = i;
-                y = j;
-                rand_seed = i + j;
-            }
-    srand(rand_seed);
-    cout << next_dir(x, y, rand()) << endl;
+
+Robot::Robot(int _round, int _width, int _height) {
+        round = _round;
+        width = _width;
+        height = _height;
+}
+
+Robot::~Robot() {
+    for (int i = 0; i < height; i++) {
+        delete[] board[i];
+    }
+    delete[] board;
+}
+
+int main(void) {
+    int round, M, N;
+    cin >> round >> M >> N;
+    Robot robot(round, N, M);
 }
